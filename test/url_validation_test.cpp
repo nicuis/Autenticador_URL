ff276e9bd8c13d9ab2d782697f1cc39df8e0a80c
Matheus2237 <mathpaulinor@gmail.com>
2024-09-22 22:37:42 -0300

    test: reajuste na lógica de validação de url para que o teste espere exceções ao invés de valor booleano

--- test/url_validation_test.cpp
+++ test/url_validation_test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 #include <string>
+#include "catch.hpp"
+#include "../src/validators/url_validator.hpp"  
 
 // TODO: Método a ser removido pois é outro o objeto de validação a ser testado 
 bool isValidUrl(const std::string& url) {
@@ -10,74 +11,74 @@ bool isValidUrl(const std::string& url) {
 TEST_CASE("Valid URLs", "[url]") {
 
     // URLs válidas
-    REQUIRE(isValidUrl("http://exemplo.com"));
-    REQUIRE(isValidUrl("https://exemplo.com"));
-    REQUIRE(isValidUrl("http://sub.exemplo.com"));
-    REQUIRE(isValidUrl("https://exemplo.com.br"));
-    REQUIRE(isValidUrl("http://exemplo.com/caminho"));
-    REQUIRE(isValidUrl("https://exemplo.com/caminho/para/recurso"));
-    REQUIRE(isValidUrl("http://exemplo.com/caminho/para/recurso?query=valor"));
-    REQUIRE(isValidUrl("https://exemplo.com/caminho?query=valor&outro=valor2"));
-    REQUIRE(isValidUrl("http://exemplo.com:8080"));
-    REQUIRE(isValidUrl("http://sub.dominio.com:8080/caminho?query=valor"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com"));
+    REQUIRE_NOTHROW(isValidUrl("http://sub.exemplo.com"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com.br"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com/caminho"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com/caminho/para/recurso"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com/caminho/para/recurso?query=valor"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com/caminho?query=valor&outro=valor2"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com:8080"));
+    REQUIRE_NOTHROW(isValidUrl("http://sub.dominio.com:8080/caminho?query=valor"));
 
     // URLs com parâmetros complexos e fragmentos
-    REQUIRE(isValidUrl("https://exemplo.com/caminho?query=valor&chave=valor#sessao1"));
-    REQUIRE(isValidUrl("https://exemplo.com/caminho?query=complexo%20query&outro=valor#header"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com/caminho?query=valor&chave=valor#sessao1"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com/caminho?query=complexo%20query&outro=valor#header"));
     
     // URLs com subdomínios e caminhos
-    REQUIRE(isValidUrl("http://sub.sub.exemplo.com"));
-    REQUIRE(isValidUrl("https://sub.sub.exemplo.com:8080"));
-    REQUIRE(isValidUrl("http://exemplo.com/"));
-    REQUIRE(isValidUrl("https://exemplo.com/#fragmento"));
-    REQUIRE(isValidUrl("http://exemplo.com/caminho/"));
-    REQUIRE(isValidUrl("https://exemplo.com/caminho/?query=valor"));
+    REQUIRE_NOTHROW(isValidUrl("http://sub.sub.exemplo.com"));
+    REQUIRE_NOTHROW(isValidUrl("https://sub.sub.exemplo.com:8080"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com/"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com/#fragmento"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com/caminho/"));
+    REQUIRE_NOTHROW(isValidUrl("https://exemplo.com/caminho/?query=valor"));
     
     // URLs com diferentes formatos de porta
-    REQUIRE(isValidUrl("http://exemplo.com:80/caminho"));
-    REQUIRE(isValidUrl("http://exemplo.com:443/caminho"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com:80/caminho"));
+    REQUIRE_NOTHROW(isValidUrl("http://exemplo.com:443/caminho"));
     
     // URLs com IPs válidos (IPv4)
-    REQUIRE(isValidUrl("http://192.168.0.1"));
-    REQUIRE(isValidUrl("http://192.168.0.1:8080"));
-    REQUIRE(isValidUrl("https://127.0.0.1"));
-    REQUIRE(isValidUrl("https://127.0.0.1:8080/caminho?query=valor"));
+    REQUIRE_NOTHROW(isValidUrl("http://192.168.0.1"));
+    REQUIRE_NOTHROW(isValidUrl("http://192.168.0.1:8080"));
+    REQUIRE_NOTHROW(isValidUrl("https://127.0.0.1"));
+    REQUIRE_NOTHROW(isValidUrl("https://127.0.0.1:8080/caminho?query=valor"));
 }
 
 TEST_CASE("Invalid URLs", "[url]") {
 
     // URLs com erros de protocolo
-    REQUIRE_FALSE(isValidUrl("htp://exemplo.com"));    // Protocolo incorreto
-    REQUIRE_FALSE(isValidUrl("httpss://exemplo.com")); // Protocolo incorreto
+    REQUIRE_THROWS_AS(isValidUrl("htp://exemplo.com"), std::invalid_argument);    // Protocolo incorreto
+    REQUIRE_THROWS_AS(isValidUrl("httpss://exemplo.com"), std::invalid_argument); // Protocolo incorreto
     
     // URLs com domínios inválidos
-    REQUIRE_FALSE(isValidUrl("http://.com"));               // Domínio vazio
-    REQUIRE_FALSE(isValidUrl("http://exemplo"));            // Falta o domínio de nível superior
-    REQUIRE_FALSE(isValidUrl("http://exemplo..com"));       // Domínio com dois pontos
-    REQUIRE_FALSE(isValidUrl("http://exemplo.com..com"));   // Domínio com dois pontos
-    REQUIRE_FALSE(isValidUrl("http://exemplo.c"));          // TLD muito curto
-    REQUIRE_FALSE(isValidUrl("http://exemplo.com:"));       // Porta sem número
-    REQUIRE_FALSE(isValidUrl("http:///exemplo.com"));       // Barras extras no protocolo
-    REQUIRE_FALSE(isValidUrl("http:// exemplo.com"));       // Espaços na URL
-    REQUIRE_FALSE(isValidUrl("ftp://exemplo.com"));         // Protocolo inválido
+    REQUIRE_THROWS_AS(isValidUrl("http://.com"), std::invalid_argument);               // Domínio vazio
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo"), std::invalid_argument);            // Falta o domínio de nível superior
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo..com"), std::invalid_argument);       // Domínio com dois pontos
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.com..com"), std::invalid_argument);   // Domínio com dois pontos
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.c"), std::invalid_argument);          // TLD muito curto
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.com:"), std::invalid_argument);       // Porta sem número
+    REQUIRE_THROWS_AS(isValidUrl("http:///exemplo.com"), std::invalid_argument);       // Barras extras no protocolo
+    REQUIRE_THROWS_AS(isValidUrl("http:// exemplo.com"), std::invalid_argument);       // Espaços na URL
+    REQUIRE_THROWS_AS(isValidUrl("ftp://exemplo.com"), std::invalid_argument);         // Protocolo inválido
     
     // URLs com portas inválidas
-    REQUIRE_FALSE(isValidUrl("http://exemplo.com:abc"));    // Porta com letras
-    REQUIRE_FALSE(isValidUrl("http://exemplo.com:999999")); // Porta fora do intervalo permitido
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.com:abc"), std::invalid_argument);    // Porta com letras
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.com:999999"), std::invalid_argument); // Porta fora do intervalo permitido
     
     // URLs com IPs inválidos (IPv4)
-    REQUIRE_FALSE(isValidUrl("http://999.999.999.999"));    // Endereço IP inválido
-    REQUIRE_FALSE(isValidUrl("http://256.256.256.256"));    // Endereço IP inválido
-    REQUIRE_FALSE(isValidUrl("http://192.168.0"));          // Endereço IP incompleto
-    REQUIRE_FALSE(isValidUrl("http://192.168.0.1.1"));      // Endereço IP com muitos octetos
+    REQUIRE_THROWS_AS(isValidUrl("http://999.999.999.999"), std::invalid_argument);    // Endereço IP inválido
+    REQUIRE_THROWS_AS(isValidUrl("http://256.256.256.256"), std::invalid_argument);    // Endereço IP inválido
+    REQUIRE_THROWS_AS(isValidUrl("http://192.168.0"), std::invalid_argument);          // Endereço IP incompleto
+    REQUIRE_THROWS_AS(isValidUrl("http://192.168.0.1.1"), std::invalid_argument);      // Endereço IP com muitos octetos
     
     // URLs malformadas
-    REQUIRE_FALSE(isValidUrl("http://"));
-    REQUIRE_FALSE(isValidUrl("https://"));
-    REQUIRE_FALSE(isValidUrl("://exemplo.com"));            // Protocolo ausente
-    REQUIRE_FALSE(isValidUrl("http:exemplo.com"));          // Protocolo sem "//"
+    REQUIRE_THROWS_AS(isValidUrl("http://"), std::invalid_argument);
+    REQUIRE_THROWS_AS(isValidUrl("https://"), std::invalid_argument);
+    REQUIRE_THROWS_AS(isValidUrl("://exemplo.com"), std::invalid_argument);            // Protocolo ausente
+    REQUIRE_THROWS_AS(isValidUrl("http:exemplo.com"), std::invalid_argument);          // Protocolo sem "//"
     
     // URLs com fragmentos incorretos
-    REQUIRE_FALSE(isValidUrl("http://exemplo.com#"));       // Fragmento vazio
-    REQUIRE_FALSE(isValidUrl("http://exemplo.com# "));      // Fragmento com espaço
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.com#"), std::invalid_argument);       // Fragmento vazio
+    REQUIRE_THROWS_AS(isValidUrl("http://exemplo.com# "), std::invalid_argument);      // Fragmento com espaço
 }
