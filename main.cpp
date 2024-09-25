6994788d4c65014f17a6ba39b9c8efc8a5e1ea71
Matheus2237 <mathpaulinor@gmail.com>
2024-09-25 16:39:15 -0300

    fix: Correção da exceção usada na main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include "src/validators/arguments_validator.h"
-#include "src/exceptions/exception_validator.h"
+#include "src/exceptions/url_validation_exception.hpp"
 #include "src/io/show_texts.hpp"
-#include <iostream>
-#include <memory>
 
 int main(int argc, char* argv[]){
 
     try {
         ArgumentValidator::validateArgs(argc, argv);
         showTexts::printText("URL válida!");
-    
-    } catch (const exception_validator& e) {
+    } catch (const url_validation_exception& e) {
         showTexts::printError(e);
     }
     return 0;
